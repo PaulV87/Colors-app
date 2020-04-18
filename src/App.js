417644fbda66c306368695146508b1fe55639714
Paul <paulventer87@gmail.com>
2020-04-18 16:25:52 +0200

    Added SinglecolorPalette component and have it showing 1 shade of a color

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 import { generatePalette } from './colorHelpers';
 
 
@@ -15,13 +16,28 @@ class App extends Component {
   render() {    
     return (     
         <Switch>
-          <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />}/>
+          <Route 
+            exact 
+            path="/" 
+            render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />}/>
           <Route 
             exact
             path="/palette/:id" 
             render={routeProps => (
-              <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} /> 
-              )}
+              <Palette 
+                palette={generatePalette(this.findPalette(routeProps.match.params.id))}
+             /> 
+            )}
+          />
+          <Route
+            exact
+            path="/palette/:paletteId/:colorId" 
+            render={(routeProps) => (
+              <SingleColorPalette 
+                colorId={routeProps.match.params.colorId} 
+                palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))} 
+              />
+            )}
           />
         </Switch>   
     );
---
+++ src/SingleColorPalette.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+
+class SingleColorPalette extends Component {
+  constructor(props) {
+    super(props);
+    this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = {}
+  }
+
+  gatherShades(palette, colorToFilterBy) {
+    let shades = [];
+    let allColors = palette.colors;
+
+    for (let key in allColors) {
+      shades = shades.concat(
+        allColors[key].filter(color => color.id === colorToFilterBy)
+      )
+    }
+
+    return shades.slice(1);
+    // return all shades of a given color
+  }
+
+  render() {
+    const colorBoxes = this._shades.map(color => (
+      <ColorBox
+        key={color.id}
+        name={color.name}
+        background={color.hex}
+        showLink = { false }
+      />
+    ));
+    return(
+      <div className="Palette">
+        <h1>Single baby</h1>
+        <div className="Palette-colors">
+          {colorBoxes}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SingleColorPalette;
