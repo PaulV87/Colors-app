bf16cfee2383b7f86e5b6dab9956aa46f0314283
Paul <paulventer87@gmail.com>
2020-04-21 15:55:03 +0200

    Added delete icon to mini palette

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -6,8 +6,9 @@ export default {
     padding: "0.5rem",
     position: "relative",
     overflow: "Hidden",
-    "&:hover": {
-      cursor: "pointer"
+    cursor: "pointer",
+    "&:hover svg": {
+       opacity: "1",
     }
   },
   colors: {
@@ -37,5 +38,21 @@ export default {
     display: "inline-block",
     margin: "0 auto",
     marginBottom: "-3.5px"
+  },
+  delete: {
+    
+  },
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    top: "0",
+    right: "0",
+    padding: "10px",
+    zIndex: "10",
+    opacity: "0",
+    transition: "all 0.3s ease-in-out",
   }
 }
