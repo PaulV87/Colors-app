35e2757c2ae7e39bda2dcd0255c59e04eeb4e6a8
Paul <paulventer87@gmail.com>
2020-05-05 22:38:32 +0200

    Fixed goback button bug

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,47 +1,45 @@
-import sizes from './sizes';
-
+import sizes from "./sizes";
 export default {
   Palette: {
     height: "100vh",
     display: "flex",
-    flexDirection: "column",
-    overflow: "hidden"
+    flexDirection: "column"
   },
   colors: {
-    height: "90%",
+    height: "90%"
   },
   goBack: {
     width: "20%",
-    height: props => props.showingFullPalette ? "25%" : "50%",
+    height: "50%",
     margin: "0 auto",
     display: "inline-block",
     position: "relative",
     cursor: "pointer",
     marginBottom: "-3.5px",
-    opacity: "1",
+    opacity: 1,
     backgroundColor: "black",
     "& a": {
-    color: "white",
-    width: "100px",
-    height: "30px",
-    position: "absolute",
-    display: "inline-block",
-    top: "50%",
-    left: "50%",
-    marginLeft: "-50px",
-    marginTop: "-15px",
-    textAlign: "center",
-    outline: "none",
-    background: "rgba(255,255,255,0.3)",
-    fontSize: "1rem",
-    lineHeight: "30px",
-    textTransform: "uppercase",
-    border: "none",
-    textDecoration: "none",
+      color: "white",
+      width: "100px",
+      height: "30px",
+      position: "absolute",
+      display: "inline-block",
+      top: "50%",
+      left: "50%",
+      marginLeft: "-50px",
+      marginTop: "-15px",
+      textAlign: "center",
+      outline: "none",
+      background: "rgba(255, 255, 255, 0.3)",
+      fontSize: "1rem",
+      lineHeight: "30px",
+      textTransform: "uppercase",
+      border: "none",
+      textDecoration: "none"
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: "33.33333%"
+      height: "33.3333%"
     },
     [sizes.down("md")]: {
       width: "50%",
@@ -49,8 +47,7 @@ export default {
     },
     [sizes.down("xs")]: {
       width: "100%",
-      height: "10%",
-    },
-
+      height: "10%"
+    }
   }
-}
+};
